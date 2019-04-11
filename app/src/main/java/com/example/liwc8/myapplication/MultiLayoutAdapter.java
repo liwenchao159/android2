16f615a48a58d17b0b2b402d28a9994bbd8ca626
李文超 <liwc8@centaline.com.cn>
2019-04-11 15:59:03 +0800

    teset

---
+++ app/src/main/java/com/example/liwc8/myapplication/MultiLayoutAdapter.java
@@ -0,0 +1,32 @@
+package com.example.liwc8.myapplication;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by liwc8 on 11/4/2019.
+ */
+
+public class MultiLayoutAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
