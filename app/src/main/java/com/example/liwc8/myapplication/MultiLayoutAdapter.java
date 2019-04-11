be09e6b9d91ec5812d0289098239d4631c8e833c
李文超 <liwc8@centaline.com.cn>
2019-04-11 16:03:16 +0800

    teset

--- app/src/main/java/com/example/liwc8/myapplication/MultiLayoutAdapter.java
+++ app/src/main/java/com/example/liwc8/myapplication/MultiLayoutAdapter.java
@@ -1,15 +1,23 @@
 package com.example.liwc8.myapplication;
 
+import android.content.Context;
+import android.content.pm.ProviderInfo;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by liwc8 on 11/4/2019.
  */
 
 public class MultiLayoutAdapter extends BaseAdapter {
-
+    private  static final  int TYPE_BOOK=0;
+    private  static final  int TYPE_APP=1;
+    private Context mContext;
+    private ArrayList<Object> mData=null;
     @Override
     public int getCount() {
         return 0;
